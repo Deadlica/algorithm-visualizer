eab2f3d340401751cf93ce5e985bd8fc6f414f9d
Deadlica <samuelgreenberg00@gmail.com>
2024-03-18 19:02:05 +0100

    REFACTOR: renamed components .tsx files for better naming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import Header from "./components/Header/page";
-import BarArray from "./components/BarArray/page";
+import Header from "./components/Header/Header";
+import BarArray from "./components/BarArray/BarArray";
 
 export default function Home() {
   return (
