b37a5d0c48c4a8bb9d64cdbc54140aa72e8a6e49
Deadlica <samuelgreenberg00@gmail.com>
2024-03-18 13:25:07 +0100

    feat: skeleton code for project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Header from "@/components/Header"
+import Container from "@/components/Container"
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Abdi was here :-)</h1>
+      <Header />
+      <Container />
+    </main>
+  );
+}
---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+function Container() {
+  return (
+    <div class="h-screen flex items-center justify-center">
+      <div class="py-4">01</div>
+      <div class="py-12">02</div>
+      <div class="py-8">03</div>
+    </div>
+  );
+}
+
+export default Container;
